8a15c0d936b04eb1606f4e36a4b778e47c79ad4a
sharath V <sharathkrml@gmail.com>
2021-12-15 11:14:25 +0530

    added settings to check metamask & connect to wallet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [account, setAccount] = useState();
+  const [metamaskStatus, setMetamaskStatus] = useState(false);
+  const checkIfWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        setMetamaskStatus(true);
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        if (accounts.length !== 0) {
+          setAccount(accounts[0]);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    checkIfWalletConnected();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="header">The Rocket Portal 🚀</div>
+        <div className="description">
+          Here,you can send your message in a 🚀 to me
+        </div>
+        {!metamaskStatus && (
+          <div className="alert">You don't have a metamask wallet</div>
+        )}
+        {account && <div className="account">Your account : {account}</div>}
+        {metamaskStatus && !account && (
+          <button onClick={connectWallet}>Connect Metamask</button>
+        )}
+      </div>
+      <div className="transactions"></div>
     </div>
   );
 }
